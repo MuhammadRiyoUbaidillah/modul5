3577cdcfddeadfd9763d4a371929fdef535c7ff9
MuhammadRiyoUbaidillah <82527896+MuhammadRiyoUbaidillah@users.noreply.github.com>
2021-05-27 03:06:22 +0700

    Create operasibilangan.java

---
+++ operasibilangan.java
@@ -0,0 +1,19 @@
+package abstrak;
+
+public abstract class operasibilangan {
+    protected double a,b,c ;
+    
+    protected abstract void setA(double a);
+    
+    protected abstract void setB(double b);
+    
+    protected abstract void setC();
+    
+    protected abstract double getA();
+    
+    protected abstract double getB();
+    
+    protected abstract double getC();
+    
+    protected abstract void tampil();    
+}
